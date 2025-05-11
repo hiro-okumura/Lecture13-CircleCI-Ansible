a97816666a4f60893b1890216827fd7a67696427
hiroyuki-okmr <146839388+hiroyuki-okmr@users.noreply.github.com>
2025-05-12 01:05:38 +0900

    sample_spec.rbに処理を追加：`ruby -v`を直接指定するのではなく`.rbenv/shims`のパスから実行する形にする

--- serverspec/spec/target/sample_spec.rb
+++ serverspec/spec/target/sample_spec.rb
@@ -10,11 +10,23 @@ describe command('git --version') do
     its(:stdout) { should match /git version 2\.47\.1/ }
 end
 
-describe package('nginx') do
-    it { should be_installed }
+describe service('mysqld') do
+    it { should be_running   }
 end
 
-describe command('ruby -v') do
-    let(:disable_sudo) { true }
-    its(:stdout) { should match 'ruby 3.2.3' }
+describe service('nginx') do
+    it { should be_running }
 end
+
+describe service('puma') do
+    it { should be_running }
+end
+
+describe port(80) do
+    it { should be_listening }
+end
+
+describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
+    its(:stdout) { should match /^200$/ }
+end
+
